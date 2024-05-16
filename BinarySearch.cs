7bd6ddcededceeaafe2a8a1c8a402bf40a603bf8
Muhdin <muhdinmussema@gmail.com>
2024-05-16 11:27:33 +0300

    Modified code

--- BinarySearch.cs
+++ BinarySearch.cs
@@ -1,38 +1,30 @@
 ﻿namespace DST_MAIN;
 public static class BinarySearch
 {
-    public static int BinarySearchs(int[] array, int target)
+    public static int BinarySearchs(int[] array, int targetValue)
     {
-
         int min = 0;
         int max = array.Length - 1;
         int guess;
         while (true)
         {
-            if (max >= min)
-            {
-                guess = (min + max) / 2;
-                if (array[guess] != target)
-                {
-                    if (array[guess] < target)
-                    {
-                        min = guess + 1;
-                    }
-                    else
-                    {
-                        max = guess - 1;
-                    }
-                }
-                else
-                {
-                    return guess;
-                }
-            }
-            else
+            if (max < min)
             {
                 return -1;
             }
+            guess = (min + max) / 2;
+            if (array[guess] == targetValue)
+            {
+                return guess;
+            }
+            else if (array[guess] < targetValue)
+            {
+                min = guess + 1;
+            }
+            else
+            {
+                max = guess - 1;
+            }
         }
     }
-
 }
