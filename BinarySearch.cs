31057cfab6623aeadb5a248010f05993d53c4239
Muhdin <muhdinmussema@gmail.com>
2024-05-16 11:22:53 +0300

    Added binary search

---
+++ BinarySearch.cs
@@ -0,0 +1,38 @@
+﻿namespace DST_MAIN;
+public static class BinarySearch
+{
+    public static int BinarySearchs(int[] array, int target)
+    {
+
+        int min = 0;
+        int max = array.Length - 1;
+        int guess;
+        while (true)
+        {
+            if (max >= min)
+            {
+                guess = (min + max) / 2;
+                if (array[guess] != target)
+                {
+                    if (array[guess] < target)
+                    {
+                        min = guess + 1;
+                    }
+                    else
+                    {
+                        max = guess - 1;
+                    }
+                }
+                else
+                {
+                    return guess;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+}
